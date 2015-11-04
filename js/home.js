4178100d300da83818cd08f0a51a59eeb4bee458
Eduardo <elopezm20@gmail.com>
2015-11-04 11:21:14 -0600

    26102015 prueba base de datos

---
+++ js/home.js
@@ -0,0 +1,36 @@
+$(window).ready(function(){
+
+    if($("#home").hasClass("ui-page-active")){
+        setTimeout ("r()", 5000);
+    }
+
+    $(window).bind("orientationchange", function(event){
+        if (event.orientation == "landscape"){
+            $("#logoG").width("20%");
+            $("#loader").css("display","none");
+        }
+        else{
+            $("#logoG").width("100%");
+            $("#loader").css("display","inherit");
+        }
+
+    });
+    $(window).trigger("orientationchange");
+
+    $("#loginBtn").on("click",function(){
+        //alert("hola");
+        login();
+    });
+});
+
+function r() { location.href="#foo" }
+
+function login(){
+    var valores = {username:$("#username").val(),pass:$("#pass").val()};
+
+
+
+    $.post('php/pruebasqlite.php',valores).done(function(data) {
+        alert(data);
+    });
+}
